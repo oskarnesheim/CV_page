262d2c88dd2ab0e2b93d2863fde82300f96c0092
Oskar Nesheim <oskarnesheim@dhcp-10-24-0-168.wlan.ntnu.no>
2023-08-24 13:56:25 +0200

    Added all work related experiences

--- public/Erfaringer.ts
+++ public/Erfaringer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { EducationItem, WorkExperienceItem, Skill } from "./interfaces";
 
 const educationData: EducationItem[] = [
@@ -5,25 +6,89 @@ const educationData: EducationItem[] = [
       degree: "Bachelor in Information Technology",
       institution: "NTNU",
       year: "2021-present",
+      grade: "3,7/5,0",
+      logo: "NTNU_logo.png",
+
     },
+    {
+      degree: "General academic competence in Natural sciences",
+      institution: "Asker VGS",
+      year: "2017-2020",
+      grade: "5.3/6.0",
+      logo: "Asker_VGS_logo.png",
+
+    }
     // Add more education items as needed
   ];
 
   const workExperienceData: WorkExperienceItem[] = [
     {
-      position: "Software Engineer",
-      company: "Tech Company Inc.",
-      duration: "2021 - Present",
+      position: "Care Worker",
+      company: "Gullhella Bo- og omsorgssenter - Asker Kommune",
+      duration: "June 2022 - August 2023",
+      logo: "Asker_Kommune.png",
+      comment: "Working as a care worker at a nursing home in Asker. Here i have learned to work in a team, and to take responsibility for others. I have also learned to work under pressure, and to be flexible"
     },
-    // Add more work experience items as needed
+    {
+      position: "Salesman",
+      company: "Lovenskiold Handel AS / Maxbo Asker",
+      duration: "May 2018 - Juli 2020",
+      logo: "Maxbo_logo.png",
+      comment: "At Maxbo I learned to work well under pressure and talk to people of all ages."
+    },
+    {
+      position: "Learning assistant - TDT4100 Object Oriented Programming",
+      company: "NTNU",
+      duration: "January 2023 - June 2023",
+      logo: "NTNU_logo.png",
+      comment: "At NTNU I have been a learning assistant for the course TDT4100 Object Oriented Programming. Here I have learned to teach others, and to explain complex concepts in a simple way. I have also gained a deeper understanding of the course material, through teaching it."
+    },
+    {
+      position: "Learning assistant - IT1901 Informatics Project 1",
+      company: "NTNU",
+      duration: "August 2023 - present",
+      logo: "NTNU_logo.png",
+      comment: "This fall I am a learning assistant for the course IT1901 Informatics Project 1. Here I will be guiding a group of students through a project, and help them with any problems they might have."
+    },
+    // {
+    //   position: "Artillery Range",
+    //   company: "Norwegian Army",
+    //   duration: "August 2020 - August 2021",
+    //   logo: "Emblem_of_the_Norwegian_Army.png",
+    //   comment: "This fall I am a learning assistant for the course IT1901 Informatics Project 1. Here I will be guiding a group of students through a project, and help them with any problems they might have."
+    // },
   ];
+
+  const otherExperiencesData: WorkExperienceItem[] = [
+    {
+      position: "Leader of the Sports Committee",
+      company: "Online",
+      duration: "February 2022 - February 2023",
+      logo: "Online_logo.png",
+      comment: "As the leader of the sports committee I have learned about leadership, and how to lead a group of people. I have experienced how it feels to take extra responsibily and how to communicate well with others."
+    },
+    {
+      position: "Member of the Bank Committee",
+      company: "Online",
+      duration: "February 2023 - present",
+      logo: "Online_logo.png",
+      comment: "As a member of the bank committee I have learned about how to manage a budget, and how to make good decisions for the organization. I also have some experience with the accounting program Fiken, which we use to manage the budget."
+    },
+    {
+      position: "Member of the Excursion Committee",
+      company: "Online",
+      duration: "March 2023 - present",
+      logo: "Online_logo.png",
+      comment: "At the excursion committee we have planned and booked a trip for the third year students at Informatics at NTNU. We have taken inn different opinions and weighed them against each other and the budget. I have learned about planning and how much work organizing a trip is."
+    },
+
+  ]
 
   const skillsData: Skill[] = [
     { name: "React", level: "Intermediate" },
     { name: "TypeScript", level: "Advanced" },
     { name: "Python", level: "Intermediate" },
     { name: "Java", level: "Advance" },
-    { name: "HTML", level: "Intermediate" },
     { name: "HTML", level: "Intermediate" },
     // Add more skills as needed
   ];
--- public/interfaces.ts
+++ public/interfaces.ts
@@ -3,12 +3,17 @@ export interface EducationItem {
   degree: string;
   institution: string;
   year: string;
+  grade?: string;
+  logo?: string;
 }
 
 export interface WorkExperienceItem {
   position: string;
   company: string;
   duration: string;
+  logo?: string;
+  comment?: string;
+
 }
 
 export interface Skill {
