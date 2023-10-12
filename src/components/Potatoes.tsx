2e539a7270d2c66b36a029545fa8c5052c83e671
Oskar Nesheim <nesheimo@gmail.com>
2023-10-12 07:46:18 +0200

    Not working very good

---
+++ src/components/Potatoes.tsx
@@ -0,0 +1,9 @@
+export default function Potatoes() {
+  return (
+    <div>
+      <h2>Potatoes</h2>
+      <p>There are many types of potatoes.</p>
+      <p>I really love them potatoes</p>
+    </div>
+  );
+}
