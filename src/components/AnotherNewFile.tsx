8119e1eb7e52cdc0b0b724c0aae7ec77c3af1f2e
Oskar Nesheim <nesheimo@gmail.com>
2023-10-12 07:48:01 +0200

    Add: made some new potatoes

---
+++ src/components/AnotherNewFile.tsx
@@ -0,0 +1,3 @@
+export default function AnotherNewFile() {
+  return <div>AnotherNewFile</div>;
+}
--- src/components/Potatoes.tsx
+++ src/components/Potatoes.tsx
@@ -4,6 +4,7 @@ export default function Potatoes() {
       <h2>Potatoes</h2>
       <p>There are many types of potatoes.</p>
       <p>I really love them potatoes</p>
+      <p>Man I really love them.</p>
     </div>
   );
 }
